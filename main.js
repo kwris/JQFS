0cb2160333838b01348aa0a96d26f3803179017d
kwris <marcin.wolski1@tlen.pl>
2019-06-17 07:46:15 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+// Accordian
+
+var action = "click";
+var speed = "500";
+
+$(document).ready(function() {
+  // Question handler
+  $("li.q").on(action, function() {
+    // Get next element
+    $(this)
+      .next()
+      .slideToggle(speed)
+      // Select all other answers
+      .siblings("li.a")
+      .slideUp();
+    // Get image for active question
+    var img = $(this).children("img");
+    // Remove 'rotate' class for all images except active one
+    $("img")
+      .not(img)
+      .removeClass("rotate");
+    // Toggle rotate class
+    img.toggleClass("rotate");
+  });
+});
